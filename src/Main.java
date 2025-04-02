3e687d17d1d0127c181b9812f046d5a03be06e0e
jlafrance3 <jlafranceiii@proton.me>
2025-04-02 15:12:29 -0400

    Created data structure for parts of speech

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 package src;
 
+import java.util.HashMap;
+
 public class Main {
     
+    public static void main(String args[]) {
+        String path = "ner_dataset2.csv";
+
+        HashMap<String, HashMap<String, Integer>> map = readFile(path);
+    }
+
+    private static HashMap<String, HashMap<String, Integer>> readFile(String path) {
+        //TODO: this
+        return new HashMap<String, HashMap<String, Integer>>();
+    }
 }
