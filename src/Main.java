5a57cd54e1ab3e2a553997fac5005ae7ec3a42f6
jlafrance3 <jlafranceiii@proton.me>
2025-04-19 09:55:11 -0400

    Added method to get a test set from a sublist of the dataset

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
+/*
+ * Natural Language Processor
+ * Authors: Ethan Kulawiak, Paul Zegarek, Jean LaFrance, Isaias Barreto
+ * 
+ */
+
 package src;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -20,9 +19,30 @@ public class Main {
         String path = "Training.txt";
         List<Sentence> dataset = readFile(path);
 
-        System.out.println(dataset.get(0).toString());
+        List<String>[] testset = getTestSet(dataset.subList(0, 100));
+        for (List<String> sentence : testset) {
+            System.out.println(String.join(" ", sentence));
+        }
     }
 
+    // Returns a 2D array of sentences and their words obtained from passed List<Sentence>
+    // Parameter should be a sublist of 1000 elements from original dataset
+    private static List<String>[] getTestSet(List<Sentence> testSentences) {
+        List<String>[] testset = new ArrayList[testSentences.size()];
+
+        // Reformat Sentence objects
+        for (int i = 0; i < testSentences.size(); i++) {
+            testset[i] = new ArrayList<String>();
+
+            for (Word word : testSentences.get(i).getWords()) {
+                testset[i].add(word.text());
+            }
+        }
+
+        return testset;
+    }
+
+    // Read file and return an array of Sentences. Each Sentence will contain an array of Word Records
     private static List<Sentence> readFile(String path) {
         List<Sentence> dataset = new ArrayList<>();     //The entire dataset in array
 
@@ -32,16 +52,16 @@ public class Main {
 
             String lineSentence;
             while((lineSentence = reader.readLine()) != null) {
-                //Build Sentence object out of elements on the read line
+                //Build Sentence object out of elements on the readline
                 Sentence sentence = new Sentence();
                 String[] pairs = lineSentence.split("\t");
 
                 for (String pair : pairs) {
                     String[] parts = pair.split("\\\\");
 
-                    // Do not include punctuation
+                    // Do not include punctuation or substrings like 's. May need to add more.
                     //https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
-                    if (!Pattern.matches("\\p{Punct}\\w*", parts[0])) {
+                    if (!Pattern.matches("\\p{Punct}\\w?", parts[0])) {
                         sentence.addWord(parts[0], parts[1]);
                     }
                 }
