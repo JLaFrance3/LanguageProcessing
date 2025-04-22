41ac1f57c91a6ebc9082d064038ed8388eedb04f
jlafrance3 <jlafranceiii@proton.me>
2025-04-22 00:02:58 -0400

    Succesfully tagged testset

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,9 @@
 package src;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,14 +18,99 @@ import java.util.List;
 public class Main {
     
     public static void main(String args[]) {
-        String path = "Training.txt";
-        List<Sentence> dataset = readFile(path);
+        String dataPath = "Training.txt";               //File path
+        List<Sentence> dataset = readFile(dataPath);    //Whole data set
 
-        HashMap<String, List<Pair>> posTagger = buildFrequencyTable(dataset);
+        List<String[][]> testSet;                       //Test set of 1000 sentence
+        HashMap<String, List<Pair>> frequencyTable;     //PoS Tag frequencies for unigram and bigram
 
-        
-        
-        
+        //Used in calculating accuracy
+        int totalWords = 0;
+        int correctTags = 0;
+        double accuracy = 100.0;
+
+        try {
+            //Create BufferedWriter for accuracy report
+            BufferedWriter writer = new BufferedWriter(new FileWriter("AccuracyReport.txt"));
+
+            // Jacknife procedure using 1000 words an iteration as a test set
+            for (int i = 0; i < dataset.size(); i += 1000) {
+                int end = Math.min(i + 1000, dataset.size());
+
+                //Create test set used to test the training
+                testSet = getTestSet(dataset.subList(i, end));
+
+                //Create training set that doesn't include test set
+                List<Sentence> trainingSet = new ArrayList<>();
+                trainingSet.addAll(dataset.subList(0, i));
+                trainingSet.addAll(dataset.subList(end, dataset.size()));
+                frequencyTable = buildFrequencyTable(trainingSet);      //Build frequency table from training set
+
+                //Tag test set
+                List<String[][]> taggedTestSet = tagTestSet(testSet, frequencyTable);
+
+                //TODO: Check accuracy
+                for (String[][] s : taggedTestSet) {
+                    for (String[] t : s) {
+                        System.out.print(t[0] + "/" + t[1]);
+                    }
+                    System.out.println();
+                }
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    // Adds tags to a test set List<String[][]>
+    // Parameter testSet should be obtained from the getTestSet() method.
+    // Parameter frequencyTable should be obtained from the buildFrequencyTable() method
+    private static List<String[][]> tagTestSet(List<String[][]> testSet, HashMap<String, List<Pair>> frequencyTable) {
+        List<String[][]> taggedTestSet = testSet;
+
+        for (String[][] testSentence : taggedTestSet) {
+            for (int j = 0; j < testSentence.length; j++) {
+                String currentWord = testSentence[j][0];
+
+                // Bigram
+                if (j > 0) {
+                    //Construct bigram key (PoS|Word) for frequency table lookup
+                    String bigramKey = testSentence[j-1][1] + "|" + currentWord;
+
+                    //Check if table contains bigram key
+                    if (frequencyTable.containsKey(bigramKey)) {
+                        //Find tag with highest frequency
+                        Pair posMatch = new Pair("", 0);
+                        for (Pair tag : frequencyTable.get(bigramKey)) {
+                            if (tag.getValue() > posMatch.getValue()) posMatch = tag;   //Swap posMatch if number of occurences is higher
+                        }
+
+                        testSentence[j][1] = posMatch.getKey();     //Add PoS tag to our test set
+                        continue;
+                    }
+                }
+
+                // Unigram
+                //Check if table contains unigram key
+                if (frequencyTable.containsKey(currentWord)) {
+                    //Find tag with highest frequency
+                    Pair posMatch = new Pair("", 0);
+                    for (Pair tag : frequencyTable.get(currentWord)) {
+                        if (tag.getValue() > posMatch.getValue()) posMatch = tag;       //Swap posMatch if number of occurences is higher
+                    }
+                    testSentence[j][1] = posMatch.getKey();         //Add PoS tag to our test set
+                }
+                else {
+                    //Default tag will be NN
+                    testSentence[j][1] = "NN";
+                }
+            }
+        }
+
+        return taggedTestSet;
     }
 
     // Returns a frequency table containing each word in
@@ -97,17 +184,19 @@ public class Main {
         return posTagger;
     }
 
-    // Returns a 2D array of sentences and their words obtained from passed List<Sentence>
+    // Returns a list of 'sentences', each a 2D array of words with an empty element for use in tagging
     // Parameter should be a sublist of 1000 elements from original dataset
-    private static List<String>[] getTestSet(List<Sentence> testSentences) {
-        List<String>[] testset = new ArrayList[testSentences.size()];
+    private static List<String[][]> getTestSet(List<Sentence> testSentences) {
+        List<String[][]> testset = new ArrayList<>();
 
         // Reformat Sentence objects
         for (int i = 0; i < testSentences.size(); i++) {
-            testset[i] = new ArrayList<String>();
+            List<Word> words = testSentences.get(i).getWords();
+            String[][] newSentence = new String[words.size()][2];
+            testset.add(newSentence);
 
-            for (Word word : testSentences.get(i).getWords()) {
-                testset[i].add(word.text());
+            for (int j = 0; j < words.size(); j++) {
+                newSentence[j][0] = words.get(j).text();
             }
         }
 
@@ -129,13 +218,13 @@ public class Main {
                 String[] pairs = lineSentence.split("\t");
 
                 for (String pair : pairs) {
-                    String[] parts = pair.split("\\");
+                    String[] parts = pair.split("\\\\");
                     sentence.addWord(parts[0], parts[1]);
                 }
 
                 dataset.add(sentence);
-                reader.close();
             }
+            reader.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
